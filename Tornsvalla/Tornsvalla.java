5524e13c28e74128a440473a18a91fb856e6a4bc
Stella Karlsson <stella.carlsson1@gmail.com>
2025-02-27 18:26:51 +0100

    La till två metoder seen() och två testflyg

--- Tornsvalla/Tornsvalla.java
+++ Tornsvalla/Tornsvalla.java
@@ -1,8 +1,20 @@
 
 public class Tornsvalla  {
     public static void main(String[] args) {
-       AircraftFactory.createPlane("Name");
-       
+        seen("ABCD", "Mosquito");
+        seen("DEFG", "Junker");
+        AircraftCollection.getAllTailcodes();   // Kollar vilka flyg som sparats i listan
     }
 
+    public static void seen(String tailcode) {
+        System.out.println("Please specify type of plane observed");
+    }
+
+    public static void seen(String tailcode, String type) {
+        AircraftFactory.createPlane(tailcode, type);
+    }
+
+    public static void aircraftInfo(String tailcode) {
+        AircraftCollection.getAircraftInfo(tailcode);
+    }
 }
