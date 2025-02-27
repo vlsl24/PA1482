1366eadcae7fcea436c3d43da80ba1a81576e107
Stella Karlsson <stella.carlsson1@gmail.com>
2025-02-27 18:43:12 +0100

    Utökade Aircraft attribut, getTailcode och info()

--- Tornsvalla/Aircraft.java
+++ Tornsvalla/Aircraft.java
@@ -1,19 +1,28 @@
+import java.util.ArrayList;
+
 class Aircraft {
 
+    private String tailcode;
     private String fullName;
+    private String shortName;
+    private String lastSeen;
+    private ArrayList<Actions> actions; 
 
-    public Aircraft(String fullName) {
-        this.fullName = fullName;
+    public Aircraft(String tailcode, String shortName) {
+        this.tailcode = tailcode;
+        this.fullName = "fullName EJ SKAPAD";   // Ej klar
+        this.shortName = shortName;
+        this.lastSeen = "DATETIME EJ SKAPAD";   // Ej klar
+        this.actions = ActionCollection.getPlaneActions(shortName);
     }
 
-
-    public String name(){
-        fullName = "test";
-        return fullName;
-      }
-
-    public String getFullName(){
-        return fullName;
+    public String getTailcode(){
+        return tailcode;
+    }
+    // Skulle kunna ändras senare
+    public void info() {
+        System.out.println(fullName + " " + tailcode);
+        System.out.println("Last seen:" + lastSeen);
     }
 
 
