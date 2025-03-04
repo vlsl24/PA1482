e52abe86109b38644b1128e2a07e6b4ebac8dcd6
Stella Karlsson <stella.carlsson1@gmail.com>
2025-03-04 17:57:54 +0100

    Utökat och lagt till funktioner i AircraftCollection

--- Tornsvalla/AircraftCollection.java
+++ Tornsvalla/AircraftCollection.java
@@ -1,22 +1,87 @@
 import java.util.ArrayList;
 
 public class AircraftCollection {
+
     private static ArrayList<Aircraft> planes = new ArrayList<>();
 
     public static void addAircraft(Aircraft aircraft) {
+
         planes.add(aircraft);
     }
 
+
+    public static void removeAircraft(String tailcode) {
+
+        if (isAircraftInList(tailcode) == true) {
+            Aircraft a = getAircraftFromList(tailcode);
+            planes.remove(a);
+        }
+    }
+
+
     public static void getAllTailcodes() {
+
         for (Aircraft a : planes) {
             System.out.println(a.getTailcode());
         }
     }
     
+
     public static void getAircraftInfo(String tailcode) {
-        // Här ska man hitta flygplan i lista
-        // och utföra aircraft.info()
-        // to be continued...
+
+        Aircraft a = getAircraftFromList(tailcode);
+
+        if (getAircraftFromList(tailcode) != null) {
+            a.info();
+        }
     }
-    
+
+
+    private static boolean isAircraftInList(String tailcode) {
+
+        int i = 0;
+        boolean tailcode_found = false;
+
+        while (tailcode_found == false && i < planes.size()) {
+            Aircraft aircraft = planes.get(i);
+            String aircraftTailcode = aircraft.getTailcode();
+
+            if (aircraftTailcode.equals(tailcode)) {
+                tailcode_found = true;
+            }
+
+            i++;
+        }
+
+        return tailcode_found;
+    }
+
+
+    private static Aircraft getAircraftFromList(String tailcode) { 
+
+        if (isAircraftInList(tailcode) == true) {
+
+            int i = 0;
+            boolean aircraft_found = false;
+            Aircraft a = null;
+
+
+            while (aircraft_found == false && i < planes.size()) {
+
+                a = planes.get(i);
+
+                if (a.getTailcode().equals(tailcode)) {
+                    aircraft_found = true;
+                }
+
+                i++;
+            }
+
+            return a;
+
+        } else {
+
+            return null;
+        }
+    }
 }
